d42b388a2013a212ea6ef03fd511ced8bf25110f
Marcelo Mariduena <marcelomariduena@protonmail.com>
2019-03-16 14:01:07 -0400

    Updates

---
+++ precache-manifest.9abd447acd8d2a8efc14e28db68c44d6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6cb9274b9b13ba213cef1757d2c1aa5",
+    "url": "/SinglePageApp/static/media/oscars1.f6cb9274.jpg"
+  },
+  {
+    "revision": "cffa8126bd14a19766e3c22e5cd0ad0e",
+    "url": "/SinglePageApp/static/media/oscarWinners3.cffa8126.jpg"
+  },
+  {
+    "revision": "4cae0712d78bac2caeae541520db75c4",
+    "url": "/SinglePageApp/static/media/oscarWinners2.4cae0712.jpg"
+  },
+  {
+    "revision": "4a4a17d6704344aee82a0f34b75c3c12",
+    "url": "/SinglePageApp/static/media/oscarWinners1.4a4a17d6.jpg"
+  },
+  {
+    "revision": "d72f1a72eacab16f00ee",
+    "url": "/SinglePageApp/static/js/runtime~main.19d82a81.js"
+  },
+  {
+    "revision": "4a487db1401f4ac2a5fc",
+    "url": "/SinglePageApp/static/js/main.9a374d45.chunk.js"
+  },
+  {
+    "revision": "f7fa9875e97ff5964b28",
+    "url": "/SinglePageApp/static/js/2.a2f3f7af.chunk.js"
+  },
+  {
+    "revision": "4a487db1401f4ac2a5fc",
+    "url": "/SinglePageApp/static/css/main.2d8d9739.chunk.css"
+  },
+  {
+    "revision": "230e9c7a43ceb42c2a9c05ef2d8465e6",
+    "url": "/SinglePageApp/index.html"
+  }
+];
